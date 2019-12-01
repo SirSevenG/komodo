94fd9070309b75017624bdde487159da823b0536
SirSevenG <ottonseven@gmail.com>
2019-12-02 03:58:06 +0700

    getinfo check

--- qa/rpc-tests/pytest_rpc/rpc_blockchain.py
+++ qa/rpc-tests/pytest_rpc/rpc_blockchain.py
@@ -4,5 +4,21 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 import pytest
-from slickrpc import Proxy
+import json
 
+
+def test_getinfo(proxy_connection):
+    with open('nodesconfig.json', 'r') as f:
+        params_dict = json.load(f)
+
+    node1_params = params_dict["node1"]
+    node2_params = params_dict["node2"]
+    rpc = proxy_connection(node1_params)
+    rpc1 = proxy_connection(node2_params)
+    pubkey = node1_params["pubkey"]
+    pubkey1 = node2_params["pubkey"]
+
+    res = rpc.getinfo()
+    assert res.get('result')
+    res = rpc1.getinfo()
+    assert res.get('result')
