4cb70e8e554f1598128eebe4ae0930a78fe47e41
SirSevenG <ottonseven@gmail.com>
2019-12-06 20:35:32 +0700

    rawtransactions calls tests

--- qa/rpc-tests/pytest_rpc/test_rawtransactions.py
+++ qa/rpc-tests/pytest_rpc/test_rawtransactions.py
@@ -8,7 +8,7 @@ from pytest_util import validate_template
 
 
 @pytest.mark.usefixtures("proxy_connection")
-class TestRawtransactions:
+class TestRawTransactions:
 
     def test_rawtransactions(self, test_params):  # create, fund, sign, send calls
         fund_schema = {
@@ -49,4 +49,96 @@ class TestRawtransactions:
         assert isinstance(sendtx, str)
 
     def test_getrawtransaction(self, test_params):  # decode, get methods
+        txschema = {
+            'type': 'object',
+            'properties': {
+                'hex': {'type': 'string'},
+                'txid': {'type': 'string'},
+                'overwintered': {'type': 'boolean'},
+                'version': {'type': 'integer'},
+                'versiongroupid': {'type': 'string'},
+                'locktime': {'type': 'integer'},
+                'expiryheight': {'type': 'integer'},
+                'vin': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'object',
+                        'properties': {
+                            'coinbase': {'type': 'string'},
+                            'txid': {'type': 'string'},
+                            'vout': {'type': 'integer'},
+                            'address': {'type': 'string'},
+                            'scriptSig': {
+                                'type': 'object',
+                                'properties': {
+                                    'asm': {'type': 'string'},
+                                    'hex': {'type': 'string'}
+                                }
+                            },
+                            'value': {'type': ['integer', 'number']},
+                            'valueSat': {'type': 'integer'},
+                            'sequence': {'type': 'integer'}
+                        }
+                    }
+                },
+                'vout': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'object',
+                        'properties': {
+                            'value': {'type': ['integer', 'number']},
+                            'valueSat': {'type': 'integer'},
+                            'interest': {'type': ['integer', 'number']},
+                            'n': {'type': 'integer'},
+                            'scriptPubKey': {
+                                'type': 'object',
+                                'properties': {
+                                    'asm': {'type': 'string'},
+                                    'hex': {'type': 'string'},
+                                    'reqSigs': {'type': 'integer'},
+                                    'type': {'type': 'string'},
+                                    'addresses': {'type': 'array', 'items': {'type': 'string'}}
+                                }
+                            }
+                        }
+                    }
+                },
+                'vjoinsplit': {'type': 'array'},
+                'valueBalance': {'type': 'number'},
+                'vShieldedSpend': {'type': 'array'},
+                'vShieldedOutput': {'type': 'array'},
+                'blockhash': {'type': 'string'},
+                'height': {'type': 'integer'},
+                'confirmations': {'type': 'integer'},
+                'rawconfirmations': {'type': 'integer'},
+                'time': {'type': 'integer'},
+                'blocktime': {'type': 'integer'}
+            }
+        }
+        scriptschema = {
+            'type': 'object',
+            'properties': {
+                'asm': {'type': 'string'},
+                'hex': {'type': 'string'},
+                'type': {'type': 'string'},
+                'reqSigs': {'type': 'integer'},
+                'address': {'type': 'string'},
+                'p2sh': {'type': 'string'},
+                'addresses': {'type': 'array',
+                              'items': {'type': 'string'}}
+            }
+        }
         rpc = test_params.get('node1').get('rpc')
+        res = rpc.listunspent()
+        txid = res[0].get('txid')
+        rawhex = rpc.getrawtransaction(txid)
+        assert isinstance(rawhex, str)
+        res = rpc.getrawtransaction(txid, 1)
+        validate_template(res, txschema)
+
+        scripthex = res.get('vout')[0].get('scriptPubKey').get('hex')
+        res = rpc.decodescript(scripthex)
+        validate_template(res, scriptschema)
+
+        res = rpc.decoderawtransaction(rawhex)
+        validate_template(res, txschema)
