ccb46513f7f53d442619b4ae700b94b71d4ebc47
SirSevenG <ottonseven@gmail.com>
2019-12-06 19:50:54 +0700

    [WIP] rawtransactions calls

--- qa/rpc-tests/pytest_rpc/test_rawtransactions.py
+++ qa/rpc-tests/pytest_rpc/test_rawtransactions.py
@@ -10,16 +10,43 @@ from pytest_util import validate_template
 @pytest.mark.usefixtures("proxy_connection")
 class TestRawtransactions:
 
-    def test_createrawtransaction(self, test_params):
-        test_values = {
-            'txid': test_params['txid'],
-            'vout': '0',
-            'address': test_params['address'],
-            'amount': '1'
+    def test_rawtransactions(self, test_params):  # create, fund, sign, send calls
+        fund_schema = {
+            'type': 'object',
+            'properties': {
+                'hex': {'type': 'string'},
+                'fee': {'type': ['integer', 'number']},
+                'changepos': {'type': ['integer', 'number']}
+            }
+        }
+        sign_schema = {
+            'type': 'object',
+            'properties': {
+                'hex': {'type': 'string'},
+                'complete': {'type': 'boolean'}
+            }
         }
         rpc = test_params.get('node1').get('rpc')
-        # [[{"txid":"9f44dc664882198b14e9a8c466d466efcdd070ccb6f57be8e2884aa11e7b2a30","vout":0}],
-        # {"RHCXHfXCZQpbUbihNHh5gTwfr7NXmJXmHi":0.01} ]
-        res = rpc.createrawtransaction([{test_values['txid'], test_values['vout']},
-                                        {test_values['address']: test_values['amount']}])
-        assert isinstance(res, str)
+        res = rpc.listunspent()
+        txid = res[0].get('txid')
+        vout = res[0].get('vout')
+        amount = res[0].get('amount') * 0.9
+        address = rpc.getnewaddress()
+        ins = [{'txid': txid, 'vout': vout}]
+        outs = {address: amount}
+
+        rawtx = rpc.createrawtransaction(ins, outs)
+        assert isinstance(rawtx, str)
+
+        fundtx = rpc.fundrawtransaction(rawtx)
+        validate_template(fundtx, fund_schema)
+
+        signtx = rpc.signrawtransaction(fundtx.get('hex'))
+        validate_template(signtx, sign_schema)
+        assert signtx['complete']
+
+        sendtx = rpc.sendrawtransaction(signtx.get('hex'))
+        assert isinstance(sendtx, str)
+
+    def test_getrawtransaction(self, test_params):  # decode, get methods
+        rpc = test_params.get('node1').get('rpc')
