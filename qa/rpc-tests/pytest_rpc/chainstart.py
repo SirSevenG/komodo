715021edd8011f0f10f1e912046f790530b02b71
SirSevenG <ottonseven@gmail.com>
2019-12-07 22:20:49 +0700

    check os in chainstart

--- qa/rpc-tests/pytest_rpc/chainstart.py
+++ qa/rpc-tests/pytest_rpc/chainstart.py
@@ -11,6 +11,10 @@ from pytest_util import create_proxy, validate_proxy
 # TODO: change env 'CHAIN' paramater to tag, use actual "coin" param from chainconfig.json instead
 def load_env_config():
     tp = {}  # test env parameters
+    if os.name == 'posix':
+        envconfig = './envconfig.json'
+    else:
+        envconfig = 'envconfig.json'
     if os.environ['CHAIN']:
         tp.update({'clients_to_start': int(os.environ['CLIENTS'])})
         tp.update({'is_bootstrap_needed': os.environ['IS_BOOTSTRAP_NEEDED']})
@@ -27,8 +31,8 @@ def load_env_config():
         tp.update({'test_wif': test_wif_list})
         tp.update({'test_address': test_addr_list})
         tp.update({'test_pubkey': test_pubkey_list})
-    elif os.path.isfile('./envconfig.json') and not os.environ['CHAIN']:
-        with open('./envconfig.json', 'r') as f:
+    elif os.path.isfile(envconfig) and not os.environ['CHAIN']:
+        with open(envconfig, 'r') as f:
             tp = json.load(f)
     else:
         raise EnvironmentError("\nNo test env configuration provided")
@@ -36,11 +40,17 @@ def load_env_config():
 
 
 def load_ac_params(asset, chain_mode='default'):
-    if os.path.isfile('./chainconfig.json'):
-        with open('./chainconfig.json', 'r') as f:
+    if os.name == 'posix':
+        chainconfig = './chainconfig.json'
+        binary_path = '../../../src/komodod'
+    else:
+        chainconfig = 'chainconfig.json'
+        binary_path = '..\\..\\..\\src\\komodo.exe'
+    if os.path.isfile(chainconfig):
+        with open(chainconfig, 'r') as f:
             jsonparams = json.load(f)
         ac = jsonparams.get(asset)  # asset chain parameters
-        ac.update({'binary_path': '../../../src/komodod'})
+        ac.update({'binary_path': binary_path})
         if chain_mode == 'REGTEST':
             ac.update({'daemon_params': ['-daemon', '-whitelist=127.0.0.1', '-regtest']})
         else:
@@ -52,10 +62,14 @@ def load_ac_params(asset, chain_mode='default'):
 
 # TODO: add coins file compatibility with create_configs func
 def create_configs(asset, node=0):
-    if not os.path.isfile('./node_' + str(node) + '/' + asset + '.conf'):
+    if os.name == 'posix':
+        confpath = './node_' + str(node) + '/' + asset + '.conf'
+    else:
+        confpath = 'node_' + str(node) + '\\' + asset + '.conf'
+    if not os.path.isfile(confpath):
         os.mkdir('node_' + str(node))
-        open('node_' + str(node) + '/' + asset + '.conf', 'a').close()
-        with open('node_' + str(node) + '/' + asset + '.conf', 'a') as conf:
+        open(confpath, 'a').close()
+        with open(confpath, 'a') as conf:
             conf.write("rpcuser=test\n")
             conf.write("rpcpassword=test\n")
             conf.write('rpcport=' + str(7000 + node) + '\n')
@@ -70,7 +84,7 @@ def main():
     for node in range(clients_to_start):  # prepare config folders
         create_configs(aschain, node)
     if env_params.get('is_bootstrap_needed'):  # bootstrap chains
-        if not os.path.isfile('./bootstrap.tar.gz'):
+        if not os.path.isfile('bootstrap.tar.gz'):
             wget.download(env_params.get('bootstrap_url'), "bootstrap.tar.gz")
             tf = tarfile.open("bootstrap.tar.gz")
             for i in range(clients_to_start):
@@ -78,12 +92,18 @@ def main():
     mode = env_params.get('chain_start_mode')
     ac_params = load_ac_params(aschain, mode)
     for i in range(clients_to_start):  # start daemons
+        if os.name == 'posix':
+            confpath = (sys.path[0] + '/node_' + str(i) + '/' + aschain + '.conf')
+            datapath = (sys.path[0] + '/node_' + str(i))
+        else:
+            confpath = (sys.path[0] + '\\node_' + str(i) + '\\' + aschain + '.conf')
+            datapath = (sys.path[0] + '\\node_' + str(i))
         cl_args = [ac_params.get('binary_path'),
                    '-ac_name=' + aschain,
-                   '-conf=' + sys.path[0] + '/node_' + str(i) + '/' + aschain + '.conf',
+                   '-conf=' + confpath,
                    '-rpcport=' + str(7000 + i),
                    '-port=' + str(6000 + i),
-                   '-datadir=' + sys.path[0] + '/node_' + str(i),
+                   '-datadir=' + datapath,
                    '-pubkey=' + env_params.get('test_pubkey')[i],
                    ]
         if i > 0:
