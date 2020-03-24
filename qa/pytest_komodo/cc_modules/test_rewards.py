2219d73c914c6623dc891aad8b4bb6e7368050c7
SirSevenG <ottonseven@gmail.com>
2020-03-25 02:49:47 +0700

    update RewardsCC test

--- qa/pytest_komodo/cc_modules/test_rewards.py
+++ qa/pytest_komodo/cc_modules/test_rewards.py
@@ -5,8 +5,183 @@
 
 import pytest
 import json
+from basic.pytest_util import validate_template, mine_and_waitconfirms, \
+                              randomstring, check_synced, wait_blocks
 from util import assert_success, assert_error, mine_and_waitconfirms, send_and_mine,\
     rpc_connect, wait_some_blocks, generate_random_string, komodo_teardown
+
+
+@pytest.mark.usefixtures("proxy_connection")
+class TestRewards:
+
+    @staticmethod
+    def new_rewardsplan(proxy, schema=None):
+        name = randomstring(4)
+        amount = '250'
+        apr = '25'
+        mindays = '0'
+        maxdays = '10'
+        mindeposit = '10'
+        res = proxy.rewardscreatefunding(name, amount, apr, mindays, maxdays, mindeposit)
+        if schema:
+            validate_template(res, schema)
+        assert res.get('result') == 'success'
+        txid = proxy.sendrawtransaction(res.get('hex'))
+        mine_and_waitconfirms(txid, proxy)
+        rewardsplan = {
+            'fundingtxid': txid,
+            'name': name
+        }
+        return rewardsplan
+
+    def test_rewardscreatefunding(self, test_params):
+        createfunding_schema = {
+            'type': 'object',
+            'properties': {
+                'result': {'type': 'string'},
+                'hex': {'type': 'string'},
+                'error': {'type': 'string'}
+            },
+            'required': ['result']
+        }
+
+        rpc = test_params.get('node1').get('rpc')
+        self.new_rewardsplan(rpc, schema=createfunding_schema)
+
+    def test_reardsaddress(self, test_params):
+        rewardsaddress_schema = {
+            'type': 'object',
+            'properties': {
+                'result': {'type': 'string'},
+                'error': {'type': 'string'},
+                'RewardsCCAddress': {'type': 'string'},
+                'RewardsCCBalance': {'type': 'number'},
+                'RewardsNormalAddress': {'type': 'string'},
+                'RewardsNormalBalance': {'type': 'number'},
+                'RewardsCCTokensAddress': {'type': 'string'},
+                'PubkeyCCaddress(Rewards)': {'type': 'string'},
+                'PubkeyCCbalance(Rewards)': {'type': 'number'},
+                'myCCAddress(Rewards)': {'type': 'string'},
+                'myCCbalance(Rewards)': {'type': 'number'},
+                'myaddress': {'type': 'string'},
+                'mybalance': {'type': 'number'}
+            },
+            'required': ['result']
+        }
+
+        rpc = test_params.get('node1').get('rpc')
+        res = rpc.rewardsaddress()
+        validate_template(res, rewardsaddress_schema)
+        assert res.get('result') == 'success'
+
+    def test_rewarsdlist(self, test_params):
+        rewadslist_schema = {
+            'type': 'array',
+            'items': {'type': 'string'}
+        }
+
+        rpc1 = test_params.get('node1').get('rpc')
+        res = rpc1.rewardslist()
+        validate_template(res, rewadslist_schema)
+
+    @staticmethod
+    def rewardsinfo_maincheck(proxy, fundtxid, schema):
+        res = proxy.rewardsinfo(fundtxid)
+        validate_template(res, schema)
+        assert res.get('result') == 'success'
+
+    def test_rewardsinfo(self, test_params):
+        rewardsinfo_schema = {
+            'type': 'object',
+            'properties': {
+                'result': {'type': 'string'},
+                'error': {'type': 'string'},
+                'fundingtxid': {'type': 'string'},
+                'name': {'type': 'string'},
+                'sbits': {'type': 'integer'},
+                'APR': {'type': 'string'},
+                'minseconds': {'type': 'integer'},
+                'maxseconds': {'type': 'integer'},
+                'mindeposit': {'type': 'string'},
+                'funding': {'type': 'string'},
+                'locked': {'type': 'string'}
+            },
+            'required': ['result']
+        }
+
+        rpc1 = test_params.get('node1').get('rpc')
+        try:
+            fundtxid = rpc1.rewardslist()[0]
+            self.rewardsinfo_maincheck(rpc1, fundtxid, rewardsinfo_schema)
+        except IndexError:
+            print('\nNo Rewards CC available on chain')
+            fundtxid = self.new_rewardsplan(rpc1).get('fundingtxid')
+            self.rewardsinfo_maincheck(rpc1, fundtxid, rewardsinfo_schema)
+
+    @staticmethod
+    def rewardsaddfunding_maincheck(proxy, fundtxid, schema):
+        name = proxy.rewardsinfo(fundtxid).get('name')
+        amount = proxy.rewardsinfo(fundtxid).get('mindeposit')  # not related to mindeposit here, just to get amount
+        res = proxy.rewardsaddfunding(name, fundtxid, amount)
+        validate_template(res, schema)
+        assert res.get('result') == 'success'
+        txid = proxy.sendrawtransaction(res.get('hex'))
+        mine_and_waitconfirms(txid, proxy)
+
+    def test_rewardsaddfunding(self, test_params):
+        addfunding_schema = {
+            'type': 'object',
+            'properties': {
+                'result': {'type': 'string'},
+                'hex': {'type': 'string'},
+                'error': {'type': 'string'}
+            },
+            'required': ['result']
+        }
+
+        rpc1 = test_params.get('node1').get('rpc')
+        try:
+            fundtxid = rpc1.rewardslist()[0]
+            self.rewardsaddfunding_maincheck(rpc1, fundtxid, addfunding_schema)
+        except IndexError:
+            print('\nNo Rewards CC available on chain')
+            fundtxid = self.new_rewardsplan(rpc1).get('fundingtxid')
+            self.rewardsaddfunding_maincheck(rpc1, fundtxid, addfunding_schema)
+
+    @staticmethod
+    def un_lock_maincheck(proxy, fundtxid, schema):
+        name = proxy.rewardsinfo(fundtxid).get('name')
+        amount = proxy.rewardsinfo(fundtxid).get('mindeposit')
+        res = proxy.rewardslock(name, fundtxid, amount)
+        validate_template(res, schema)
+        assert res.get('result') == 'success'
+        locktxid = proxy.sendrawtransaction(res.get('hex'))
+        mine_and_waitconfirms(locktxid, proxy)
+        res = proxy.rewardsunlock(name, fundtxid, locktxid)
+        validate_template(res, schema)
+        assert res.get('result') == 'success'
+        unlocktxid = proxy.sendrawtransaction(res.get('hex'))
+        mine_and_waitconfirms(unlocktxid, proxy)
+
+    def test_lock_unlock(self, test_params):
+        lock_unlock_schema = {
+            'type': 'object',
+            'properties': {
+                'result': {'type': 'string'},
+                'hex': {'type': 'string'},
+                'error': {'type': 'string'}
+            },
+            'required': ['result']
+        }
+
+        rpc1 = test_params.get('node1').get('rpc')
+        try:
+            fundtxid = rpc1.rewardslist()[0]
+            self.un_lock_maincheck(rpc1, fundtxid, lock_unlock_schema)
+        except IndexError:
+            print('\nNo Rewards CC available on chain')
+            fundtxid = self.new_rewardsplan(rpc1).get('fundingtxid')
+            self.un_lock_maincheck(rpc1, fundtxid, lock_unlock_schema)
 
 
 @pytest.mark.usefixtures("proxy_connection")
