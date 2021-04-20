bb2dad59cc5c3a2169f9a90e465cd201fc8ebc05
SirSevenG <44422309+SirSevenG@users.noreply.github.com>
2021-04-20 16:33:08 +0700

    testgmp - debuggin'

---
+++ src/testgmp.c
@@ -0,0 +1,14 @@
+/* Override any GCC internal prototype to avoid an error.
+   Use char because int might match the return type of a GCC
+   builtin and then its argument prototype would still apply.  */
+#ifdef __cplusplus
+extern "C"
+#endif
+char __gmpn_sub_n ();
+int
+main ()
+{
+return __gmpn_sub_n ();
+  ;
+  return 0;
+}
